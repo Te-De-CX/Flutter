488ca1fae318cc66744051deac7ecc832443db19
teslim <tunjiheritage@gmail.com>
2025-03-07 02:14:13 +0100

    done

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,21 +1,11 @@
-allprojects {
+buildscript {
+    ext.kotlin_version = '1.7.10' // Update Kotlin version if needed
     repositories {
         google()
         mavenCentral()
     }
-}
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    dependencies {
+        classpath 'com.android.tools.build:gradle:7.2.2' // Update to the latest stable version
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
 }
